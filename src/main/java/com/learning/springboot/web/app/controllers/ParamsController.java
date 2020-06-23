b65844e0e8af339f58294d779a509c373ccc0851
aflores01 <karunsane@gmail.com>
2020-06-23 18:48:14 -0500

    Multiples valores en HTTP GET con HTTPSERVLETREQUEST y con REQUESTPARAM

--- src/main/java/com/learning/springboot/web/app/controllers/ParamsController.java
+++ src/main/java/com/learning/springboot/web/app/controllers/ParamsController.java
@@ -1,5 +1,7 @@
 package com.learning.springboot.web.app.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,25 @@ public class ParamsController {
 	@GetMapping("/string")
 	public String param(@RequestParam(name = "texto", required=false, defaultValue="Some value...") String texto,Model model) {
 		model.addAttribute("resultado", "El parametro enviado es: " + texto);
+		return "params/ver";
+	}
+	
+	@GetMapping("/mix-params")
+	public String param(@RequestParam() String saludo, @RequestParam Integer numero,Model model) {
+		model.addAttribute("resultado", "El parametro enviado es: '" + saludo + "' y el número es: '" + numero + "'");
+		return "params/ver";
+	}
+	
+	@GetMapping("/mix-params-request")
+	public String param(HttpServletRequest request,Model model) {
+		String saludo = request.getParameter("saludo");
+		Integer numero = null;
+		try {
+		numero = Integer.parseInt(request.getParameter("numero"));
+		}catch(NumberFormatException e){
+			numero = 0;
+		}
+		model.addAttribute("resultado", "El parametro enviado es: '" + saludo + "' y el número es: '" + numero + "'");
 		return "params/ver";
 	}
 }
