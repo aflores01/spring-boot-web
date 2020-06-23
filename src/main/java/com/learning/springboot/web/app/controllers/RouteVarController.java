569a1d9b40f169ee33aa35cb5172c4f0513c5864
aflores01 <karunsane@gmail.com>
2020-06-23 18:59:33 -0500

    Pasar parametro por @PathVariable

---
+++ src/main/java/com/learning/springboot/web/app/controllers/RouteVarController.java
@@ -0,0 +1,18 @@
+package com.learning.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vars")
+public class RouteVarController {
+	@GetMapping("/string/{text}")
+	public String vars(@PathVariable String text, Model model) {
+		model.addAttribute("titulo","Recibir parametros de la ruta (@PathVariable)");
+		model.addAttribute("resultado","El texto enviado en la ruta es: " + text);
+		return "vars/ver";
+	}
+}
