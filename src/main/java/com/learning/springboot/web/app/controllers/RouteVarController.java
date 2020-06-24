ae159468b67a6ede2fc4526f4434f7593a45be70
aflores01 <karunsane@gmail.com>
2020-06-23 19:13:59 -0500

    Enviar y recibir valores por @PathVariable

--- src/main/java/com/learning/springboot/web/app/controllers/RouteVarController.java
+++ src/main/java/com/learning/springboot/web/app/controllers/RouteVarController.java
@@ -9,10 +9,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/vars")
 public class RouteVarController {
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("titulo","Enviar parametros de la ruta (@PathVariable)");
+		return "vars/index";
+	}
+	
 	@GetMapping("/string/{text}")
 	public String vars(@PathVariable String text, Model model) {
 		model.addAttribute("titulo","Recibir parametros de la ruta (@PathVariable)");
 		model.addAttribute("resultado","El texto enviado en la ruta es: " + text);
+		return "vars/ver";
+	}
+	
+	@GetMapping("/string/{text}/{numero}")
+	public String vars(@PathVariable String text,@PathVariable int numero, Model model) {
+		model.addAttribute("titulo","Recibir parametros de la ruta (@PathVariable)");
+		model.addAttribute("resultado","El texto enviado en la ruta es: " + text
+				+ " Y el número es: " + numero);
 		return "vars/ver";
 	}
 }
